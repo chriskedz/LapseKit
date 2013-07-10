141959300a7ed9ec80481fb4a813de349dd63293
Caleb Davenport <caleb@seesaw.co>
2013-07-10 15:26:50 -0700

    Add some comments.

--- LapseKit/EVLTimelineIdentifier.h
+++ LapseKit/EVLTimelineIdentifier.h
@@ -8,12 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Identifier for the featured clip timeline.
+ */
 NSString *EVLFeaturedTimelineIdentifier(void);
 
+/**
+ Timeline identifier for clips that the given user has liked.
+ */
 NSString *EVLLikesTimelineIdentifierForUserWithID(NSUInteger userID);
 
+/**
+ Timeline identifier for clips that were created by the given user.
+ */
 NSString *EVLOwnedClipsTimelineIdentifierForUserWithID(NSUInteger userID);
 
+/**
+ Timeline identifier for clips that the given user has contributed to.
+ */
 NSString *EVLContributedClipsTimelineIdentifierForUserWithID(NSUInteger userID);
 
+/**
+ Timeline ideitifier for clips that the given user is following.
+ */
 NSString *EVLFriendsTimelineIdentifierForUserWithID(NSUInteger userID);
